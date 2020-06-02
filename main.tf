ebab98234d44aca35044a590c0d7564fe0be0d69
root <jgmitz@gmail.com>
2020-06-02 15:01:49 +0000

    Tue Jun 02 2020 - Committed from Codeherent platform

--- main.tf
+++ main.tf
@@ -2,18 +2,17 @@ resource "google_compute_instance" "raddit" {
   name         = "raddit-instance"
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
-
   # boot disk specifications
   boot_disk {
     initialize_params {
       image = "raddit-base" // use image built with Packer
     }
   }
-
   # networks to attach to the VM
   network_interface {
     network = "default"
-    access_config { // use ephemaral public IP
+    access_config {
+      // use ephemaral public IP
     }
   }
 }
@@ -29,8 +28,15 @@ resource "google_compute_firewall" "raddit" {
   network = "default"
   allow {
     protocol = "tcp"
-    ports    = ["9292"]
+    ports = [
+      "9292",
+    ]
   }
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = [
+    "0.0.0.0/0",
+  ]
 }
 
+module "" {}
+
+terraform {}
